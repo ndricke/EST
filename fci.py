617e7579a0dec2f725edf65436818fda270c5487
ndricke <nathan.d.ricke@gmail.com>
2018-12-11 16:50:40 -0500

    term evaluation for H implemented, but needs to account for unrestricted case

--- fci.py
+++ fci.py
@@ -3,9 +3,11 @@ from scipy.special import comb
 from itertools import dropwhile
 from copy import copy
 import sys
+
+from embed import c1c2
 """
-1. Need to implement this to work properly over both spins
-2. Create direct product of alpha and beta spin determinants to make full determinant space
+1. need h_a, h_b, V_aa, V_ab, V_bb for systems with different numbers of spin
+   for the moment, I will assume that N[0] == N[1]
 """
 class FCI:
 
@@ -33,31 +35,33 @@ class FCI:
                 string_list[spin].append(next_excitation) #append before excitation to include ground state
                 next_excitation = self.exciteString(next_excitation, self.N[spin])
 
-        print(string_list)
-        print(len(string_list))
-        print(len(string_list[0]))
-        sys.exit(-1)
+        num_str_a = len(string_list[0]) #number of alpha strings
+        num_str_b = len(string_list[1]) #number of beta strings
+        self.string_ab = np.zeros((num_str_a*num_str_b, self.K*2)) #array for combined alpha and beta strings
+        for i in range(num_str_a):
+            for j in range(num_str_b):
+                row = j + i*num_str_b
+                self.string_ab[row,:self.K] = string_list[0][i]
+                self.string_ab[row,self.K:] = string_list[1][j]
 
         for i in range(KcN2):
             for j in range(i+1):
                 H[i,j] = self.compareString(i,j)
                 H[j,i] = H[i,j]
 
+        print(H)
+
     def exciteString(self, string, N):
         next_string = copy(string)
-        print(string)
         rightmost_1 = next(dropwhile(lambda x: string[x] != 1, reversed(range(self.K))))
-        print(rightmost_1)
 
         #if the highest 1 is not at the top, move it up 1
         if rightmost_1 != self.K-1:
-            print("not at top")
             next_string[rightmost_1] = 0
             next_string[rightmost_1+1] = 1
             return next_string
 
         else:
-            print("Hit the top!")
             #if the highest 1 is at the top, look for the highest 0
             rightmost_0 = next(dropwhile(lambda x: string[x] != 0, reversed(range(self.K))))
             #if all the ones are at the top, return error (cannot excite further)
@@ -73,57 +77,95 @@ class FCI:
             next_string[next_1+1: next_1+1+self.K-rightmost_0] = 1
             return next_string
 
-    def calcStringdex(self, i):
-        return i//self.KcN[spin], i % self.KcN[spin]
+    ## This was from when I had the alpha and beta strings as separate lists. I may go back to it
+    #def calcStringdex(self, i):
+    #    return i//self.KcN[spin], i % self.KcN[spin]
 
     def excitOperators(self, str_1, str_2):
         """
-
+        params:
+            str_1, str_2 : (np.array) occupations for excitation strings
         ----
         returns:
-            lowering_dex : (int) index for lowering operator
-            raising_dex : (int) index for raising operator
+             (list of tuples) tuples containing indices for (creation, destruction) operators
         """
-        str_dif_a = str_1a - str_2a
-        lowering_dex = np.where(str_dif_a == 1)[0]
-        raising_dex = np.where(str_dif_a == -1)[0]
-        return lowering_dex, raising_dex
+        str_dif = str_1 - str_2
+        lowering_dex = np.where(str_dif == 1)[0]
+        raising_dex = np.where(str_dif == -1)[0]
+        return zip(raising_dex, lowering_dex)
 
     def compareString(self, stringdex_1, stringdex_2):
+        """
+        params:
+            stringdex_1, stringdex_2 : (int) index for excitation strings in string_list
+        returns:
+            (int) FCI Hamiltonian coefficient
+        """
+        credes_op = self.excitOperators(self.string_ab[stringdex_1], self.string_ab[stringdex_2])
 
-        str_ind_1a, str_ind_1b = self.calcStringdex(stringdex_1)
-        str_ind_2a, str_ind_2b = self.calcStringdex(stringdex_2)
-        str_1a, str_1b = string_list[str_ind_1a], string_list[str_ind_1b]
-        str_2a, str_2b = string_list[str_ind_2a], string_list[str_ind_2b]
-
-        al_a, ar_a = self.excitOperators(str_1a, str_2a)
-        al_b, ar_b = self.excitOperators(str_1b, str_2b)
-
-        excit_count = len(al_a) + len(al_b) #number of excitations between alpha and beta strings
+        excit_count = len(credes_op) #number of excitations between alpha and beta strings
         if excit_count > 2:
             return 0.0
         elif excit_count == 2:
-            return self.doubleExcitation(al_a, al_b, ar_a, ar_b, str_1a, str_1b, str_2a, str_2b)
+            return self.doubleExcitation(credes_op)
         elif excit_count == 1:
-            return self.singleExcitation(al_a, al_b, ar_a, ar_b, str_1a, str_1b, str_2a, str_2b)
+            return self.singleExcitation(stringdex_1, stringdex_2, credes_op)
         elif excit_count == 0:
-            return self.evalEnergy(str_1a, str_1b)
+            return self.evalEnergy(stringdex_1)
         else:
             raise ValueError("Excitation count is not a whole number")
 
-    def evalEnergy(self, string_a, string_b):
+    def evalEnergy(self, stringdex): #if there are no excitations, only 1 string need be passed
+        """
+        params:
+            stringdex : (list) excitation string index
+        returns:
+            (double) : E, the energy of this string
+        """
+        ex_string = self.string_ab[stringdex,:]
+        E = 0.0
+        occ_ind = np.where(ex_string == 1)[0]
+        for a in occ_ind:
+            E += self.h[a,a]
+        for a,b in itertools.product(occ_ind,occ_ind):
+            E += 0.5*(self.V[a,a,b,b]-self.V[a,b,b,a]) ## check: is this correct for the unrestricted basis?
+        return E
+
+    def singleExcitation(self, stringdex_1, stringdex_2, credes_op):
+        """
+        params:
+            str_1 : (list) excitation string
+        returns:
+            (double) : E, the energy of this string
+        """
+        occ_ind_1 = np.where(self.string_ab[stringdex_1] == 1)[0]
+        occ_ind_2 = np.where(self.string_ab[stringdex_2] == 1)[0]
+
+        E = 0.0
+        E += self.h[credes_op[0][0], credes_op[0][1]] #only one 1-particle term
+
+        m = credes_op[0]  #creation operator
+        p = credes_op[1]  #destruction operator
+        for n in occ_ind_1: ## check: is this correct given each string has different occupations?
+            E += self.V[m,p,n,n] - self.V[m,n,n,p]
+        return E
 
 
-    def singleExcitation(self, str_1a, str_1b, str_2a, str_2b):
+    def doubleExcitation(self, credes_op):
+        m = credes_op[0][0]
+        n = credes_op[0][1]
+        p = credes_op[1][0]
+        q = credes_op[1][1]
+        return V[m,p,n,q] - V[m,q,n,p]
 
-    def doubleExcitation(self):
-        pass
 
 
-
-
-        #return next_string
 
 
 if __name__ == "__main__":
-    bob = FCI(6, [3,3], h=1, V=1)
+    n = 6
+    U = 2.0
+    na, nb = 3, 3
+
+    h, V = c1c2.hubb(n, U, pbc=True)
+    fci_6 = FCI(n, [na,nb], h=h, V=V)
